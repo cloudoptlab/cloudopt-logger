094b041a0dcb3aa05ae00ccd617c0ef639f19223
t-baby <myts.designer@gmail.com>
2018-10-18 11:42:31 +0800

    Initializing project

---
+++ src/main/kotlin/net/cloudopt/logger/Hello.kt
@@ -0,0 +1,7 @@
+package net.cloudopt.logger
+
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/net/cloudopt/HelloTest.kt
@@ -0,0 +1,8 @@
+package net.cloudopt
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
